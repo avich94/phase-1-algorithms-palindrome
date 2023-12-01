87c823a2f6c016e8cb1f048ed18782795ee6ee30
Austin Vichich <avichich4@gmail.com>
2023-12-01 13:42:10 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-function isPalindrome(word) {
   // Write your algorithm here
-}
+  function isPalindrome(word) {
+    let reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+  }
 
 /* 
   Add your pseudocode here
+  Okay at first I tried to iterate using a for of loop with no success.
+  I then realized, since the word is spelled the same
+  forward and backwards, that if I could just reverse the string.
+  I then would be able to see if the reverse string was equal to the input.
+
+  assign a variable to the word reversed
+    return the input with === and see if it is equl to the reversed word.
+
+  
 */
 
 /*
-  Add written explanation of your solution here
+  This function takes an argument of a word which is a string with no spaces, special characters etc.
+  First I declare a variable of reversedWord to the value of the word. I then add the split string method,
+  which splits the characters in the string by the characters. The reversed method is then applied to reverse the order of the
+  string. The join method then takes the split reversed characters and joins them back into a new string that is reversed.
+  The last step is comparing the word that was inputted to the newly reversed word. Using the === sign results in a boolean result of 
+  either true or false. Here we test words that are palindromes and words that are not to verify if this function provides the correct
+  results.
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +33,11 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("kayak"))
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("horse"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
